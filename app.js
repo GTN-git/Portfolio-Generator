91ddf5af0b10728b18da3e98575c0699a0208437
George Ngo <georgengo@msn.com>
2021-08-21 10:37:17 -0700

    main

--- app.js
+++ app.js
@@ -148,27 +148,3 @@ promptUser()
   });
 
 
-<<<<<<< HEAD
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-    will be uncommented in lesson 4
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./dist/index.html', pageHTML, err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Page created! Check out index.html in this directory to see it!');
-    
-      fs.copyFile('./src/style.css', './dist/style.css', err => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log('Style sheet copied successfully!');
-      });
-    });
-=======
->>>>>>> feature/updated-template
